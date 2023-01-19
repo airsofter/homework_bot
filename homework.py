4534fdca39de89cdf306ede079d0fb481e948b04
Денис Набатов <nabatoffdenis@yandex.ru>
2023-01-19 16:48:11 +0300

    config

--- homework.py
+++ homework.py
@@ -1,35 +1,21 @@
-import os
+from os import path
 import logging
+import logging.config
 import time
 from http import HTTPStatus
 
 import requests
-from dotenv import load_dotenv
 import telegram
 
+from config import (PRACTICUM_TOKEN, TELEGRAM_TOKEN,
+                    TELEGRAM_CHAT_ID, RETRY_PERIOD,
+                    ENDPOINT, HEADERS,
+                    HOMEWORK_VERDICTS,)
 
-load_dotenv()
-# Не получилось вынести настройки в отдельный файл из за специфики тестов
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+logging.config.fileConfig('logger.conf', disable_existing_loggers=False)
 
-RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-# На удаление этой переменной тесты тоже ругаются(
-HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    filename='program.log',
-    level=logging.DEBUG
-)
+logger = logging.getLogger(__name__)
 
 
 def check_tokens() -> bool:
@@ -37,17 +23,16 @@ def check_tokens() -> bool:
     check = True
     if not PRACTICUM_TOKEN:
         check = False
-        logging.critical('Отсутствуют переменная PRACTICUM_TOKEN')
+        logger.critical('Отсутствуют переменная PRACTICUM_TOKEN')
         raise Exception('Отсутствует PRACTICUM_TOKEN')
     if not TELEGRAM_TOKEN:
         check = False
-        logging.critical('Отсутствуют переменная TELEGRAM_TOKEN')
+        logger.critical('Отсутствуют переменная TELEGRAM_TOKEN')
         raise Exception('Отсутствует TELEGRAM_TOKEN')
     if not TELEGRAM_CHAT_ID:
         check = False
-        logging.critical('Отсутствуют переменная TELEGRAM_CHAT_ID')
+        logger.critical('Отсутствуют переменная TELEGRAM_CHAT_ID')
         raise Exception('Отсутствует TELEGRAM_CHAT_ID')
-
     return check
 
 
@@ -58,45 +43,43 @@ def send_message(bot, message):
             chat_id=TELEGRAM_CHAT_ID,
             text=message,
         )
-        logging.debug('Сообщение отправлено')
+        logger.debug('Сообщение отправлено')
     except Exception as error:
-        logging.error(f'Не удалось отправить сообщение. {error}')
+        logger.error(f'Не удалось отправить сообщение. {error}')
 
 
-def get_api_answer(timestamp) -> dict:
+def get_api_answer(timestamp):
     """Запрос к API."""
     try:
         response = requests.get(ENDPOINT,
                                 headers=HEADERS,
                                 params={'from_date': timestamp})
     except Exception as error:
-        logging.error(f'Ошибка при запросе к основному API: {error}. '
-                      f'Данные: ENDPOINT={ENDPOINT}, headers={HEADERS}, '
-                      f'from_date={timestamp}')
+        logger.error(f'Ошибка при запросе к основному API: {error}. '
+                     f'Данные: ENDPOINT={ENDPOINT}, '
+                     f'headers={HEADERS}, '
+                     f'from_date={timestamp}')
 
     if response.status_code != HTTPStatus.OK:
-        logging.error(f'Нет доступа к API. Код ответа: {response.status_code}')
+        logger.error(f'Нет доступа к API. Код ответа: {response.status_code}')
         raise requests.RequestException('Нет доступа к API')
     try:
-        response.json()
+        return response.json()  # вынес в try возвращение json
     except Exception as error:
-        logging.error(f'Ответ сервера не в формате json. {error}')
-
-    return response.json()
+        logger.error(f'Ответ сервера не в формате json. {error}')
 
 
 def check_response(response) -> list:
     """Проверка ответа API на соответствие документации."""
     if type(response) is not dict:
-        logging.error('Объект response не является словарем')
+        logger.error('Объект response не является словарем')
         raise TypeError('Объект response не является словарем')
     if 'homeworks' not in response.keys():
-        logging.error('Нет ключа "homeworks"')
+        logger.error('Нет ключа "homeworks"')
         raise KeyError('Нет ключа "homeworks"')
     if type(response['homeworks']) is not list:
-        logging.error('Данные ответа не являются списком')
+        logger.error('Данные ответа не являются списком')
         raise TypeError('Данные ответа не являются списком')
-
     return response['homeworks']
 
 
@@ -105,15 +88,15 @@ def parse_status(homework) -> str:
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
     if homework_name is None:
-        logging.error('Отсутствует ключ "homework_name". '
-                      f'homework_name={homework_name}')
+        logger.error('Отсутствует ключ "homework_name". '
+                     f'homework_name={homework_name}')
         raise KeyError('Отсутствует ключ "homework_name"')
     if homework_status is None:
-        logging.error('Отсутствует ключ "homework_status". '
-                      f'homework_status={homework_status}')
+        logger.error('Отсутствует ключ "homework_status". '
+                     f'homework_status={homework_status}')
         raise KeyError('Отсутствует ключ "homework_status"')
     if homework_status not in HOMEWORK_VERDICTS.keys():
-        logging.error(
+        logger.error(
             f'Недокументированный статус. homework_status={homework_status}'
         )
         raise KeyError('Недокументированный статус')
@@ -137,7 +120,7 @@ def main():
                     message = parse_status(homeworks[0])
                     send_message(bot, message)
                 else:
-                    logging.debug('Статус работы не изменился')
+                    logger.debug('Статус работы не изменился')
                     raise Exception('Статус работы не изменился')
 
             except Exception as error:
